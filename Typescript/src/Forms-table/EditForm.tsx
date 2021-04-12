da09fe99a4dd297aaa239aa3bd483de1d033880a
Vishnu T <vishnuthamiz96@gmail.com>
2021-04-12 22:46:23 +0530

    react router redirects

---
+++ Typescript/src/Forms-table/EditForm.tsx
@@ -0,0 +1,12 @@
+
+
+function EditForm() {
+    return (
+        <div>
+            <p>Edit form works!!!</p>
+            
+        </div>
+    )
+}
+
+export default EditForm
---
+++ Typescript/src/Navbar.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom"
+import EditForm from "./Forms-table/EditForm"
+import EmployeeForm from "./Forms-table/EmployeeForm"
+import TableDisplay from "./Forms-table/TableDisplay"
+
+
+function Navbar() {
+    return (
+        <div>
+
+            <div>
+                <nav className="navbar navbar-light bg-light">
+
+                    <BrowserRouter>
+                        <ul>
+                            <Link to="/">
+                                Home
+                                 </Link>
+                            <Link to="/createform">
+                                Create Employee
+                                </Link>
+
+                            <Link to="/viewEmployee">
+                                View employee
+                                 </Link>
+
+                                 
+                        </ul>
+
+                        <Switch>
+                            <Route path="/createform">
+                                <EmployeeForm />
+                            </Route>
+                            <Route path="/viewEmployee">
+                                <TableDisplay />
+                            </Route>
+
+                            <Route path={`/editform/:id`} component={EditForm} />
+                        </Switch>
+                    </BrowserRouter>
+
+                </nav>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
